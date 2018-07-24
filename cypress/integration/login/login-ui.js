ac166346bb545a3f11bbfe957dd92a2c441df972
Valerie Jane <valeriejanethoma@gmail.com>
2018-07-23 22:30:27 -0400

    add family member login with ui

--- cypress/integration/login/login-ui.js
+++ cypress/integration/login/login-ui.js
@@ -4,9 +4,25 @@ describe("Loggin in", () => {
   })
 
   context("logs in as family member", () => {
-    it.("logs in with correct credentials", () => {
-      cy.get(".nav-bar > .login > .family-login").click();
+    it("logs in with correct credentials", () => {
+      cy.get("[data-cy=familyLogin]")
+        .click({force:true})
+
+      cy.get("[data-cy=userEmail]")
+        .type("shyla@mail.com")
+
+      cy.get("[data-cy=userPass")
+        .type("shyla")
+      
+      cy.get("[data-cy=userSubmit]")
+        .click()  
+
+      cy.location("pathname").should("eq", "/users/home");  
+
     });
   });
 
 });
+
+
+
