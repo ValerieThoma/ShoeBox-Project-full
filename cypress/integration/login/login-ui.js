ee48e58c7e3dd5137a74a8a55346ae21099a8dc7
Valerie Jane <valeriejanethoma@gmail.com>
2018-07-23 22:50:02 -0400

    add user login test with bad credentials

--- cypress/integration/login/login-ui.js
+++ cypress/integration/login/login-ui.js
@@ -19,10 +19,30 @@ describe("Loggin in", () => {
 
       cy.location("pathname").should("eq", "/users/home");  
 
-    });
   });
 
+    it("logs in with incorrect credentials", () => {
+      cy.get("[data-cy=familyLogin]")
+        .click({ force: true })
+
+      cy.get("[data-cy=userEmail]")
+        .type("shyla@mail.com")
+
+      cy.get("[data-cy=userPass")
+        .type("wrongpassword")
+
+      cy.get("[data-cy=userSubmit]")
+        .click()
+
+      cy.get(".swal-modal")
+        .should('exist')
+      
+      cy.get(".swal-title")
+        .should("contain", "Your password is incorrect")
+    });
+
+  });
+
+
+
 });
-
-
-
